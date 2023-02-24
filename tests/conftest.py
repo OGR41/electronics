357a2efc361766d01a8a1a088827d157cfc4f411
OGR41 <122967956+OGR41@users.noreply.github.com>
2023-02-24 22:52:31 +1200

    .

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,3 @@ import pytest
 @pytest.fixture()
 def product():
     return Products("Чехол", 1500, 15)
-
-
-# @pytest.fixture()
-# def get_discount():
-#     product1 = Products("Смартфон", 10000, 20)
-#     return product1
